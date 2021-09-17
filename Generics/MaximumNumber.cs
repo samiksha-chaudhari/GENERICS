42e7e5e6df10a922c753c6c2cf945b4ad3c73f13
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-17 23:25:21 +0530

    updating commands

--- Generics/MaximumNumber.cs
+++ Generics/MaximumNumber.cs
@@ -6,8 +6,10 @@ namespace Generics
 {
     class MaximumNumber
     {
+        //creating method to find maximum interger
         public static int MaximumInteger(int firstValue, int secondValue, int thirdValue)
         {
+            //comparing values to find maximum integer 
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
                 return firstValue;
@@ -23,8 +25,10 @@ namespace Generics
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
 
+        //creating method to find maximum float number
         public static double MaximumFloat(double firstValue, double secondValue, double thirdValue)
         {
+            //comparing values to find maximum float number 
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
             {
                 return firstValue;
@@ -40,8 +44,10 @@ namespace Generics
             throw new Exception("firstNumber,secondNumber and thirdNumber are same");
         }
 
+        //creating method to find maximum string
         public static string MaximumString(string firstString, string secondString, string thirdString)
         {
+            //comparing values to find maximum string 
             if (firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) > 0 ||
                 firstString.CompareTo(secondString) >= 0 && firstString.CompareTo(thirdString) > 0 ||
                 firstString.CompareTo(secondString) > 0 && firstString.CompareTo(thirdString) >= 0)
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -4,29 +4,29 @@ namespace Generics
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)// main method
         {
             Console.WriteLine("Welcome To Generics Program");
             /*Console.WriteLine("Maximum Number is :");
-              int output = MaximumNumber.MaximumInteger(50, 60, 70);
+              int output = MaximumNumber.MaximumInteger(50, 60, 70); // calling maximumInterger()
               Console.WriteLine(output);
 
-              double doubleoutput = MaximumNumber.MaximumFloat(30.7, 28.47, 98.56);
+              double doubleoutput = MaximumNumber.MaximumFloat(30.7, 28.47, 98.56);// calling maximumFloat()
               Console.WriteLine(doubleoutput);
 
-              string stringoutput = MaximumNumber.MaximumString("80","26","69");
+              string stringoutput = MaximumNumber.MaximumString("80","26","69");// calling maximumString()
               Console.WriteLine(stringoutput);
             */
 
-            int[] intArray = { 112, 344, 432, 634, 488 };
+            int[] intArray = { 112, 344, 432, 634, 488 }; //creating int array
             GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
             generic.PrintMaxValue();
 
-            double[] doubleArray = { 120.2, 34.8, 46.3, 568.5, 686.78 };
+            double[] doubleArray = { 120.2, 34.8, 46.3, 568.5, 686.78 }; //creating double array
             GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
             genericDouble.PrintMaxValue();
 
-            string[] stringArray = { "apple", "mango", "orange", "banana", "grapes" };
+            string[] stringArray = { "apple", "mango", "orange", "banana", "grapes" }; //creating string array
             GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
             genericString.PrintMaxValue();
         }
