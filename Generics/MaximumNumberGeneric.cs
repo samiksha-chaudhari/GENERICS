2917b45e67cddb065893732f298e3117f5067b94
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-20 22:15:35 +0530

    updating

--- Generics/MaximumNumberGeneric.cs
+++ Generics/MaximumNumberGeneric.cs
@@ -4,15 +4,28 @@ using System.Text;
 
 namespace Generics
 {
-    public class GenericMaximum<T> where T : IComparable
+ /// <summary>
+ /// creating method to find maximum 
+ /// </summary>
+ /// <typeparam name="T"></typeparam>
+    public class GenericMaximum<T> 
 
     {
         public T[] value;
+        /// <summary>
+        /// this method is created for initializing the construtor of instance variable
+        /// </summary>
+        /// <param name="value"></param>
         public GenericMaximum(T[] value)
         {
             this.value = value;
         }
 
+        /// <summary>
+        /// this mmethod is created for sorting the values
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public T[] Sort(T[] values)
         {
             Array.Sort(values);
@@ -20,19 +33,30 @@ namespace Generics
 
         }
 
+        /// <summary>
+        /// this method is created for finding max for given type
+        /// </summary>
+        /// <param name="values"></param>
+        /// <returns></returns>
         public T MaxValue(params T[] values)
         {
             var sorted_values = Sort(values);
-
             return sorted_values[sorted_values.Length - 1];
         }
 
+        /// <summary>
+        /// this method is created for calling maxvalue()
+        /// </summary>
+        /// <returns></returns>
         public T MaxMethod()
         {
             var max = MaxValue(this.value);
             return max;
         }
 
+        /// <summary>
+        /// to print max value
+        /// </summary>
         public void PrintMaxValue()
         {
             var max = MaxValue(this.value);
